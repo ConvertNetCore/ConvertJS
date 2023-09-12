367afa73251f83481de60270e2685d6cd7eba530
tahai211 <taiich1972@gmail.com>
2023-09-12 21:20:28 +0700

    haitx update

--- ConvertJS/Services/RulesServices/RulesService.cs
+++ ConvertJS/Services/RulesServices/RulesService.cs
@@ -20,7 +20,7 @@ namespace ConvertJS.Services.RulesServices
 
         //Todo
         public Task<List<AdsAccountDTO>> GetAllAccount(string accessTokenInfo, string cookie);
-        public Task<List<GetRuleDTO>> GetRule(string accessTokenInfo, string cookie);
+        public Task<List<GetRuleDTO>> GetRule(string accessTokenInfo, string cookie,string id_tkqc);
 
     }
     public class RulesService : IRulesService
@@ -308,7 +308,7 @@ namespace ConvertJS.Services.RulesServices
 
         }
 
-        public async Task<List<AdsAccountDTO>> GetAllAccount(string accessTokenInfo, string cookie,string id_tkqc)
+        public async Task<List<AdsAccountDTO>> GetAllAccount(string accessTokenInfo, string cookie)
         {
             try
             {
@@ -319,21 +319,22 @@ namespace ConvertJS.Services.RulesServices
                     UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
                 };
                 var client = new RestClient(options);
-                var request = new RestRequest("https://adsmanager-graph.facebook.com/v15.0/act_"+id_tkqc+"/adrules_library?access_token="+ accessTokenInfo + "&__cppo=1&__activeScenarioIDs=%5B%5D&__activeScenarios=%5B%5D&__interactionsMetadata=%5B%5D&_reqName=adaccount%2Fadrules_library&_reqSrc=AdsRuleListDataManager&_sessionID=85b63ea04270095&date_format=U&fields=%5B%22account_id%22%2C%22created_time%22%2C%22entity_type%22%2C%22evaluation_spec%22%2C%22execution_spec%22%2C%22id%22%2C%22is_for_account%22%2C%22name%22%2C%22schedule_spec%22%2C%22status%22%2C%22updated_time%22%2C%22created_by%7Bid%2C%20name%7D%22%5D&include_headers=false&limit=250&locale=en_GB&method=get&pretty=0&suppress_http_code=1&xref=f1ed4b64eabbfd8&", Method.Get);
-                request.AddHeader("authority", "adsmanager-graph.facebook.com");
-                request.AddHeader("accept", "*/*");
+                var request = new RestRequest("https://graph.facebook.com/v14.0/me/adaccounts?limit=50&fields=users,name,account_status,account_id,owner_business,created_time,next_bill_date,currency,adtrust_dsl,timezone_name,timezone_offset_hours_utc,business_country_code,disable_reason,adspaymentcycle{threshold_amount},balance,is_prepay_account,owner,all_payment_methods{pm_credit_card{display_string,exp_month,exp_year,is_verified},payment_method_direct_debits{address,can_verify,display_string,is_awaiting,is_pending,status},payment_method_paypal{email_address},payment_method_tokens{current_balance,original_balance,time_expire,type}},total_prepay_balance,insights.date_preset(maximum){spend}&access_token=" +
+                  accessTokenInfo +//.Access_token +
+                  "&summary=1&locale=en_US", Method.Get);
+                request.AddHeader("authority", "graph.facebook.com");
+                request.AddHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
                 request.AddHeader("accept-language", "en-US,en;q=0.9");
-                request.AddHeader("content-type", "application/x-www-form-urlencoded");
-                request.AddHeader("origin", "https://adsmanager.facebook.com");
-                request.AddHeader("referer", "https://adsmanager.facebook.com/");
+                request.AddHeader("cache-control", "max-age=0");
                 request.AddHeader("cookie", cookie);
-                request.AddHeader("sec-ch-ua", "\"Google Chrome\";v=\"113\", \"Chromium\";v=\"113\", \"Not=A?Brand\";v=\"24\"");
+                request.AddHeader("sec-ch-ua", "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"");
                 request.AddHeader("sec-ch-ua-mobile", "?0");
                 request.AddHeader("sec-ch-ua-platform", "\"Windows\"");
                 request.AddHeader("sec-fetch-dest", "document");
-                request.AddHeader("sec-fetch-mode", "cors");
-                request.AddHeader("sec-fetch-site", "same-site");
-                request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36");
+                request.AddHeader("sec-fetch-mode", "navigate");
+                request.AddHeader("sec-fetch-site", "none");
+                request.AddHeader("sec-fetch-user", "?1");
+                request.AddHeader("upgrade-insecure-requests", "1");
                 var body = @"";
                 request.AddParameter("text/plain", body, ParameterType.RequestBody);
                 RestResponse response = await client.ExecuteAsync(request);
@@ -384,7 +385,7 @@ namespace ConvertJS.Services.RulesServices
             }
         }
 
-        public async Task<List<GetRuleDTO>> GetRule(string accessTokenInfo, string cookie)
+        public async Task<List<GetRuleDTO>> GetRule(string accessTokenInfo, string cookie, string id_tkqc)
         {
             try
             {
@@ -395,22 +396,21 @@ namespace ConvertJS.Services.RulesServices
                     UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
                 };
                 var client = new RestClient(options);
-                var request = new RestRequest("https://graph.facebook.com/v14.0/me/adaccounts?limit=50&fields=users,name,account_status,account_id,owner_business,created_time,next_bill_date,currency,adtrust_dsl,timezone_name,timezone_offset_hours_utc,business_country_code,disable_reason,adspaymentcycle{threshold_amount},balance,is_prepay_account,owner,all_payment_methods{pm_credit_card{display_string,exp_month,exp_year,is_verified},payment_method_direct_debits{address,can_verify,display_string,is_awaiting,is_pending,status},payment_method_paypal{email_address},payment_method_tokens{current_balance,original_balance,time_expire,type}},total_prepay_balance,insights.date_preset(maximum){spend}&access_token=" +
-                  accessTokenInfo +//.Access_token +
-                  "&summary=1&locale=en_US", Method.Get);
-                request.AddHeader("authority", "graph.facebook.com");
-                request.AddHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+                var request = new RestRequest("https://adsmanager-graph.facebook.com/v15.0/act_" + id_tkqc + "/adrules_library?access_token=" + accessTokenInfo + "&__cppo=1&__activeScenarioIDs=%5B%5D&__activeScenarios=%5B%5D&__interactionsMetadata=%5B%5D&_reqName=adaccount%2Fadrules_library&_reqSrc=AdsRuleListDataManager&_sessionID=85b63ea04270095&date_format=U&fields=%5B%22account_id%22%2C%22created_time%22%2C%22entity_type%22%2C%22evaluation_spec%22%2C%22execution_spec%22%2C%22id%22%2C%22is_for_account%22%2C%22name%22%2C%22schedule_spec%22%2C%22status%22%2C%22updated_time%22%2C%22created_by%7Bid%2C%20name%7D%22%5D&include_headers=false&limit=250&locale=en_GB&method=get&pretty=0&suppress_http_code=1&xref=f1ed4b64eabbfd8&", Method.Get);
+                request.AddHeader("authority", "adsmanager-graph.facebook.com");
+                request.AddHeader("accept", "*/*");
                 request.AddHeader("accept-language", "en-US,en;q=0.9");
-                request.AddHeader("cache-control", "max-age=0");
+                request.AddHeader("content-type", "application/x-www-form-urlencoded");
+                request.AddHeader("origin", "https://adsmanager.facebook.com");
+                request.AddHeader("referer", "https://adsmanager.facebook.com/");
                 request.AddHeader("cookie", cookie);
-                request.AddHeader("sec-ch-ua", "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"");
+                request.AddHeader("sec-ch-ua", "\"Google Chrome\";v=\"113\", \"Chromium\";v=\"113\", \"Not=A?Brand\";v=\"24\"");
                 request.AddHeader("sec-ch-ua-mobile", "?0");
                 request.AddHeader("sec-ch-ua-platform", "\"Windows\"");
                 request.AddHeader("sec-fetch-dest", "document");
-                request.AddHeader("sec-fetch-mode", "navigate");
-                request.AddHeader("sec-fetch-site", "none");
-                request.AddHeader("sec-fetch-user", "?1");
-                request.AddHeader("upgrade-insecure-requests", "1");
+                request.AddHeader("sec-fetch-mode", "cors");
+                request.AddHeader("sec-fetch-site", "same-site");
+                request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36");
                 var body = @"";
                 request.AddParameter("text/plain", body, ParameterType.RequestBody);
                 RestResponse response = await client.ExecuteAsync(request);
